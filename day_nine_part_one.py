75f0d57aa21e2730d239654ec2f608d3902dfcab
LorneDot <lornedauterman@gmail.com>
2022-12-16 17:52:09 -0800

    day nine part one

--- day_nine_part_one.py
+++ day_nine_part_one.py
@@ -4,17 +4,34 @@ x_y_map = {'U':1,'D':1,'R':0,'L':0}
 def move_rope(head_position,tail_position,move,tail_positions): 
     for i in range(move[1]):
         head_position[x_y_map[move[0]]] += move_map[move[0]]
-        print("head "+ str(head_position))
-        if abs(head_position[0] - tail_position[0]) > 1:
+
+        if abs(head_position[1] - tail_position[1]) == 2 and abs(head_position[0] - tail_position[0]) == 2:
+            tail_position[1] += ((head_position[1] - tail_position[1])/abs(head_position[1] - tail_position[1]))
             tail_position[0] += ((head_position[0] - tail_position[0])/abs(head_position[0] - tail_position[0]))
             tail_positions.append(str(str(int(tail_position[0]))) + " " + str(int(tail_position[1])))
             continue
-        if abs(head_position[1] - tail_position[1]) > 1:
+
+        if abs(head_position[1] - tail_position[1]) == 2 and abs(head_position[0] - tail_position[0]) == 1:
             tail_position[1] += ((head_position[1] - tail_position[1])/abs(head_position[1] - tail_position[1]))
+            tail_position[0] += ((head_position[0] - tail_position[0])/abs(head_position[0] - tail_position[0]))
+            tail_positions.append(str(str(int(tail_position[0]))) + " " + str(int(tail_position[1])))
+            continue
+
+        if abs(head_position[1] - tail_position[1]) == 1 and abs(head_position[0] - tail_position[0]) == 2:
+            tail_position[1] += ((head_position[1] - tail_position[1])/abs(head_position[1] - tail_position[1]))
+            tail_position[0] += ((head_position[0] - tail_position[0])/abs(head_position[0] - tail_position[0]))
+            tail_positions.append(str(str(int(tail_position[0]))) + " " + str(int(tail_position[1])))
+            continue
+
+        if abs(head_position[0] - tail_position[0]) == 2:
+            tail_position[0] += ((head_position[0] - tail_position[0])/abs(head_position[0] - tail_position[0]))
             tail_positions.append(str(str(int(tail_position[0]))) + " " + str(int(tail_position[1])))
             continue
         
-        print("tail "+ str(str(int(tail_position[0]))) + " " + str(int(tail_position[1])))
+        if abs(head_position[1] - tail_position[1]) == 2:
+            tail_position[1] += ((head_position[1] - tail_position[1])/abs(head_position[1] - tail_position[1]))
+            tail_positions.append(str(str(int(tail_position[0]))) + " " + str(int(tail_position[1])))
+            continue
 
 if __name__ == '__main__':
 
